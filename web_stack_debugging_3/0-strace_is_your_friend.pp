3658c08e0f48f34b1384dcfdb7bfb7fa6138e02e
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2022-12-16 12:13:03 -0800

    update

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #Using strace to find out why Apache is returning a 500 error
 exec {'quick bug fix':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
     path    => shell,
 }
 
